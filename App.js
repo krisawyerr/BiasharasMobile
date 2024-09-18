38a770ae165b476235b92582d112a2eeb1026be7
krisawyerr <Krisawyerrr@gmail.com>
2024-09-18 08:25:30 -0700

    finished base layout

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Dashboard from './screens/Dashboard/Dashboard';
+import Backtest from './screens/Backtest/Backtest';
+import Diary from './screens/Diary/Diary';
+import Profile from './screens/Profile/Profile';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Drawer = createDrawerNavigator()
+const Tabs = createBottomTabNavigator()
+
+function DashboardLayout() {
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name='Stats' component={Dashboard}/>
+      <Tabs.Screen name='Trades' component={Dashboard}/>
+      <Tabs.Screen name='Form' component={Dashboard}/>
+    </Tabs.Navigator>
+  )
+}
+
+function BacktestLayout() {
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name='Backtester' component={Backtest}/>
+      <Tabs.Screen name='Stats' component={Backtest}/>
+    </Tabs.Navigator>
+  )
+}
+
+function DiaryLayout() {
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name='Post' component={Diary}/>
+      <Tabs.Screen name='Write Post' component={Diary}/>
+    </Tabs.Navigator>
+  )
+}
+
+function SectionDrawers() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name='Dashboard' component={DashboardLayout}/>
+      <Drawer.Screen name='Backtest' component={BacktestLayout}/>
+      <Drawer.Screen name='Diary' component={DiaryLayout}/>
+      <Drawer.Screen name='Profile' component={Profile}/>
+    </Drawer.Navigator>
+  ); 
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <NavigationContainer>
+        <SectionDrawers />
+      </NavigationContainer>
+    </>
   );
 }
 
