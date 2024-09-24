ca8abc787990ce376660dd1ca6e0d9098e07b240
krisawyerr <Krisawyerrr@gmail.com>
2024-09-23 18:45:16 -0700

    updated form for desc to be multiline

--- components/Dashboard/TradeForm.js
+++ components/Dashboard/TradeForm.js
@@ -6,7 +6,7 @@ import CustomButton from "../../components/UI/CustomButton";
 import CustomTitle from "../../components/UI/CustomTitle";
 import { formatDate, formatPrice } from "../../utils/format";
 import CustomKeyboardScrollView from "../../components/UI/CustomKeyboardScrollView";
-import { View } from "react-native";
+import { TextInput, View } from "react-native";
 
 export default function TradeForm({type, assetName, setAssetName, tradingSession, setTradingSession, result, setResult, pnl, setPnL, openTime, setOpenTime, closeTime, setCloseTime, description, setDescription, onSubmit, onCancel}) {
     return (
@@ -19,7 +19,7 @@ export default function TradeForm({type, assetName, setAssetName, tradingSession
             {result.value && <CustomTextInput value={pnl} onChangeText={(e) => setPnL({ value: formatPrice(e), isFilled: true })} placeholder="Profit / Loss" />}
             <CustomTextInput value={openTime} onChangeText={(e) => setOpenTime({ value: formatDate(e), isFilled: true })} placeholder="Date Opened (YYYY-MM-DD)" keyboardType="number-pad" maxLength={10}/>
             <CustomTextInput value={closeTime} onChangeText={(e) => setCloseTime({ value: formatDate(e), isFilled: true })} placeholder="Date Closed (YYYY-MM-DD)" keyboardType="number-pad" maxLength={10}/>
-            <CustomTextInput value={description} onChangeText={(e) => setDescription({ value: e, isFilled: true })} placeholder="Description"/>
+            <CustomTextInput multiline={true} value={description} onChangeText={(e) => setDescription({ value: e, isFilled: true })} placeholder="Description"/>
             <View style={{flexDirection: "row", gap: 10}}>
                 {type === "edit" && <CustomButton halfWidth={true} backgroundColor={GlobalColors.colors.primary400} color={GlobalColors.colors.primary100} title="Cancel" onPress={onCancel} />}
                 <CustomButton halfWidth={true} backgroundColor={GlobalColors.colors.primary400} color={GlobalColors.colors.primary100} title={type === "new" ? "Submit" : "Edit"} onPress={onSubmit} />
--- components/UI/CustomTextInput.js
+++ components/UI/CustomTextInput.js
@@ -3,13 +3,13 @@ import React from 'react';
 import { StyleSheet, TextInput, View, InputAccessoryView, Platform, Keyboard, Pressable, Text } from 'react-native';
 import { GlobalColors } from "../../constants/colors";
 
-export default function CustomTextInput({ value, onChangeText, placeholder, halfWidth, password, keyboardType }) {
+export default function CustomTextInput({ value, onChangeText, placeholder, halfWidth, password, keyboardType, multiline }) {
     const inputAccessoryViewID = 'uniqueID';
 
     return (
         <View style={[styles.rootContainer, {flex: halfWidth && 1 }]}>
             {value.value && <AnimatedInputPlaceholder placeholderText={placeholder} value={value} />}
-            <TextInput style={[ styles.formInput, { borderBottomColor: value.isFilled ? GlobalColors.colors.primary400 : 'red'}]} value={value.value} onChangeText={onChangeText} placeholder={placeholder} placeholderTextColor={value.isFilled ? GlobalColors.colors.primary400 : "red"} secureTextEntry={password} keyboardType={keyboardType} returnKeyType="done" blurOnSubmit={true} inputAccessoryViewID={Platform.OS === 'ios' && keyboardType === "number-pad" ? inputAccessoryViewID : undefined}/>
+            <TextInput style={[ styles.formInput, { borderBottomColor: value.isFilled ? GlobalColors.colors.primary400 : 'red'}]} multiline={multiline} value={value.value} onChangeText={onChangeText} placeholder={placeholder} placeholderTextColor={value.isFilled ? GlobalColors.colors.primary400 : "red"} secureTextEntry={password} keyboardType={keyboardType} returnKeyType="done" blurOnSubmit={true} inputAccessoryViewID={Platform.OS === 'ios' && keyboardType === "number-pad" ? inputAccessoryViewID : undefined}/>
             {Platform.OS === 'ios' && keyboardType === "number-pad" && (
                 <InputAccessoryView nativeID={inputAccessoryViewID}>
                     <View style={styles.accessory}>
